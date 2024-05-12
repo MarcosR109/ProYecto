8a23becdfc0d171c896d4d988d2721ca38dbf02b
MarcosR <daw09.2024@gmail.com>
2024-05-12 11:54:56 +0200

    Código terminado, he subido también las imágenes

--- views/users/register.php
+++ views/users/register.php
@@ -17,12 +17,13 @@
     <style>
         .logo {
             width: 50px;
-            scale:2.5;
+            scale: 2.5;
             height: 50px;
         }
     </style>
 </head>
-<section class="vh-100" style="background-color: #eee;">
+<body style="background-image: url('<?= ROOT_URL . 'assets/images/516667ldsdl.jpg' ?>'); background-size: cover;">
+<section class="vh-100" style="background-image: url('<?= ROOT_URL . 'assets/images/516667ldsdl.jpg' ?>'); background-size: cover;">
     <div class="container h-100">
         <div class="row d-flex justify-content-center align-items-center h-100">
             <div class="col-lg-12 col-xl-11">
@@ -30,46 +31,44 @@
                     <div class="card-body p-md-5">
                         <div class="row justify-content-center">
                             <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
-
                                 <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign up</p>
-
                                 <form class="mx-1 mx-md-4" method="post" action="<?= ROOT_URL . 'users/register' ?>">
-
                                     <div class="d-flex flex-row align-items-center mb-4">
                                         <i class="fas fa-user fa-lg me-3 fa-fw"></i>
                                         <div data-mdb-input-init class="form-outline flex-fill mb-0">
-                                            <input type="text" name="name" id="name" class="form-control form-control-lg"/>
+                                            <input type="text" name="name" id="name"
+                                                   class="form-control form-control-lg"/>
                                             <label class="form-label" for="name">Nombre</label>
                                         </div>
                                     </div>
                                     <div class="d-flex flex-row align-items-center mb-4">
                                         <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
                                         <div data-mdb-input-init class="form-outline flex-fill mb-0">
-                                            <input type="email" name="email" id="email" class="form-control form-control-lg"/>
+                                            <input type="email" name="email" id="email"
+                                                   class="form-control form-control-lg"/>
                                             <label class="form-label" for="email">Email</label>
                                         </div>
                                     </div>
                                     <div class="d-flex flex-row align-items-center mb-4">
                                         <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
                                         <div data-mdb-input-init class="form-outline flex-fill mb-0">
-                                            <input type="password" name="password" id="password" class="form-control form-control-lg"/>
+                                            <input type="password" name="password" id="password"
+                                                   class="form-control form-control-lg"/>
                                             <label class="form-label" for="password">Contraseña</label>
                                         </div>
                                     </div>
                                     <div class="d-flex flex-row align-items-center mb-4">
                                         <i class="fas fa-key fa-lg me-3 fa-fw"></i>
                                         <div data-mdb-input-init class="form-outline flex-fill mb-0">
-                                            <input type="text" name="ciudad" id="ciudad" class="form-control form-control-lg"/>
+                                            <input type="text" name="ciudad" id="ciudad"
+                                                   class="form-control form-control-lg"/>
                                             <label class="form-label" for="ciudad">Ciudad</label>
                                         </div>
                                     </div>
                                     <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
                                         <input class="btn btn-primary" name="submit" type="submit" value="Registrarse"/>
                                     </div>
-
-
                                 </form>
-
                             </div>
                             <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
                                 <img src="<?php echo ROOT_URL . "assets/images/403403mtsdl.jpg" ?>"
@@ -84,3 +83,4 @@
         </div>
     </div>
 </section>
+</body>
